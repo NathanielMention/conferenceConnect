ee2e6124d84fb40c0417d9329a4e6270e1f88daf
Victor Wu <victorwu820@gmail.com>
2023-02-16 12:39:20 -0500

    Added a time picker button (does not work yet), and will store the events in firebase, you also need to do npm install react-time-picker

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,4 +1,3 @@
-
 import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
@@ -8,7 +7,16 @@ import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
+import TimePicker from 'react-time-picker'
+import { firebaseDB } from "../utils/FirebaseConfig.ts";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 const locales = {
     "en-US": require("date-fns/locale/en-US"),
@@ -21,60 +29,94 @@ const localizer = dateFnsLocalizer({
     locales,
 });
 
-const events = [
-    // {
-    //     title: "Big Meeting",
-    //     allDay: true,
-    //     start: new Date(2021, 6, 0),
-    //     end: new Date(2021, 6, 0),
-    // },
-    // {
-    //     title: "Vacation",
-    //     start: new Date(2021, 6, 7),
-    //     end: new Date(2021, 6, 10),
-    // },
-    // {
-    //     title: "Conference",
-    //     start: new Date(2021, 6, 20),
-    //     end: new Date(2021, 6, 23),
-    // },
+const event = [
+    {
+        title: "Big Meeting",
+        // allday: false,
+        start: new Date(2023, 1, 5, 7, 0, 0),
+        end: new Date(2023, 1, 5,  10, 30, 0),
+    },
+    {
+        title: "Vacation",
+        start: new Date(2023, 2, 10),
+        end: new Date(2023, 2, 10),
+    },
+    {
+        title: "Conference",
+        start: new Date(2023, 2, 20),
+        end: new Date(2023, 2, 23),
+    },
 ];
 
 function CalendarView() {
-    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
-    const [allEvents, setAllEvents] = useState(events);
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: ""});
+    const [allEvents, setAllEvents] = useState(event);
+    const [newTitle, setNewTitle] = useState("");
+    const [newStart, setNewStart] = useState("");
+    const [newEnd, setNewEnd] = useState("");
+    const usersCollectionRef = collection(firebaseDB, "events");
 
-    function handleAddEvent() {
-        for (let i=0; i<allEvents.length; i++){
-          const d1 = new Date (allEvents[i].start);
-          const d2 = new Date(newEvent.start);
-          const d3 = new Date(allEvents[i].end);
-          const d4 = new Date(newEvent.end);
-          if (
-            ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
-              (d4 <= d3) )
-            )
-          {   
-              alert("CLASH"); 
-              break;
-          }
-        }
-      
-        
-        setAllEvents([...allEvents, newEvent]);
+    
+
+    const [startTime, setStartTime] = useState('10:00')
+    const [endTime, setEndTime] = useState('11:00')
+
+    const handleStartTimeChange = (value) => {
+        setStartTime(value)
     }
+
+    const handleEndTimeChange = (value) => {
+        setEndTime(value)
+    }
+
+    // function handleAddEvent() {
+    //     // setNewEvent({...newEvent, title: newTitle})
+    //     const createTask = async () => {
+    //         await addDoc(usersCollectionRef, { event: newEvent })
+    //     };
+    //     setAllEvents([...allEvents, newEvent]);
+        
+        
+    // }
+    const createTask = async () => {
+        await addDoc(usersCollectionRef, { event: newEvent })
+        setAllEvents([...allEvents, newEvent]);
+    };
     return (
+        
         <div className="CalendarView">
+            
             <h1>Calendar</h1>
             <h2>Add New Event</h2>
             <div>
-                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
-                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
-                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
-                <button style={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} 
+                    value={newEvent.title} 
+                    onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} 
+                    // onChange={(e) => setNewTitle(e.target.value )} 
+                />
+
+                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} 
+                    selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} 
+                />
+                <DatePicker placeholderText="End Date" style={{ marginRight: "10px" }} 
+                    selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })}
+                />
+                
+                <p>Start Time</p>
+                <TimePicker placeholderText = "Start Time" value={startTime} onChange={handleStartTimeChange} />
+                <p>End Time</p>
+                <TimePicker value={endTime} onChange={handleEndTimeChange} />
+                <button style={{ marginTop: "10px" }} onClick={ createTask}>
                     Add Event
                 </button>
-            </div><Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+            </div>
+            <Calendar 
+                localizer={localizer} 
+                events={allEvents} 
+                startAccessor="start" 
+                endAccessor="end" 
+                style={{ height: 500, margin: "50px" }} 
+            />
         </div>
       
     );
